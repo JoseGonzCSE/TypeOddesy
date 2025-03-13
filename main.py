2ddc9e5315fc98d9452168c21dd74597291949c3
Andres <116050946+JoseGonzCSE@users.noreply.github.com>
2025-03-13 16:21:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import time
 
 #Puts several dictionaries into a list
 #Put these into dictionaries and then put them into a list 
@@ -41,21 +42,65 @@ def verify_player_input():
         else:
             print("please Enter a valid input (y/n)")
 
+def check_player_input(quote:str, user_input:str):
+    quote=list(quote)
+    user_input=list(user_input)
+    quote_len=len(quote)
+    user_input_len=len(user_input)
+    smallest_len=min(quote_len, user_input_len)
 
+
+
+    correct=0
+    accuracy=0
+    #use the smallest length to iterate through
+    for char in range(smallest_len):
+        if quote[char]==user_input[char]:
+            correct+=1
+    
+    #find difference in extra charcters
+
+    if user_input_len>quote_len:
+        correct=-user_input-quote_len
+    accuracy=round(correct/quote_len*100,2)
+    print (correct)
+    print(f'accuracy:{accuracy}%')    
+
+    
+    
+    
+
+    
 
 
 def main():
     file_name = 'data.csv'
     data = read_csv_file(file_name)
-    print("This is the final step")
+    print("Welcome! Let's play a game of quotes! Type the following quote! \n") 
 
     play_again=False
     while play_again==False:
         content=get_random_quote(data)
         print(content)
 
-        quote=content.get("name")
+        quote=content.get("favorite_quote")
         print(quote)
+        start_time=time.perf_counter()
+        user_input=input("Input: ")
+        end_time=time.perf_counter()
+        time_elapsed=round(end_time-start_time,2)
+
+        # have to get words per minute, use user input, go through array, if space-> thats stopping point = one word 
+            # wait if its just sapce that dictates words, count how many spaces = thats how many words there are, + 1 for the last word 
+            #Double spaces get fucked 
+        total_words= user_input.count(" ") +1
+        words_per_minute= total_words/time_elapsed *60
+
+        check_player_input(quote, user_input)
+        print(f'Time Elapsed: {time_elapsed} seconds')
+        print(f"Words/minute: {words_per_minute} ")
+        
+        
 
 
 
